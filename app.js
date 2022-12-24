448b0d914b9a7879877b3d354ba96ab432c6c9dd
safahaimour <98284222+safahaimour@users.noreply.github.com>
2022-12-24 21:00:45 +0200

    json

---
+++ app.js
@@ -0,0 +1,18 @@
+let safa_information = [
+
+    myPersonInformation = {
+        "name" : "safa",
+        "age" : 32,
+        "family_members" : ["father,mother,daughter"], // list
+         "courses" :{
+            "course_one" : "Automation",
+            "course_two" : "python",
+         } //object
+    },
+
+    myLocationInformation = {
+        "city" : "amman" , 
+        "country" : "jordan",
+         "work_place" : "Amman",
+    }
+]
